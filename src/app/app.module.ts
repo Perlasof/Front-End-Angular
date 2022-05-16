15aee67e247a0a9cc7fd32d88f18d61d02a2fb58
Perla <94577878+Perlasof@users.noreply.github.com>
2022-05-15 21:54:11 -0300

    Porfolio con componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogosComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    ProyectosComponent,
+    FooterComponent,
+    AcercaDeComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
